44eb1931d83e4821ce7a4fd8b414ac6e641049c1
mirkoperillo <mirko.perillo@gmail.com>
2019-07-30 09:15:41 +0200

    remove noisy endpoints from swagger ui page

--- src/main/java/it/smartcommunitylab/smartchainbackend/config/SwaggerConfig.java
+++ src/main/java/it/smartcommunitylab/smartchainbackend/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package it.smartcommunitylab.smartchainbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,7 +16,8 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any()).build();
     }
 
